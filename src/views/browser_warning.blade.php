38085357de043bcea3bfb8f904cbd163d4cf4f54
Sinup <vsinup@gmail.com>
2017-01-03 17:05:09 +0200

    warning fix

--- src/views/browser_warning.blade.php
+++ src/views/browser_warning.blade.php
@@ -3,10 +3,11 @@
 <head>
 	<meta charset="utf-8">
 	<title>Warning!</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 </head>
 <body>
-	<div>
-		<img src="../../img/icon.png" />
+	<div style="text-transform: uppercase; text-align: center; font-size: 25px; font-family: Roboto; margin-top: 100px">
+		<img src="/img/icon.png" />
 		<p>Please use <strong>a modern version<br /> of your browser</strong> to see this site.</p>
 	</div>
 </body>
